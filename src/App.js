4ad8a48682c4ba2d1cd129e96347cf526f2be485
Chandra Badan Singh <chandings@gmail.com>
2022-06-04 12:01:10 +0530

    Update App.js
    fixed the how to play sections language errors

--- src/App.js
+++ src/App.js
@@ -20,12 +20,12 @@ function App() {
             <button className="app-help-close-btn" onClick={hideHelpDiv}><i className="gg-close"></i></button> 
           </div>
           <h3>How to play Womble?</h3>
-          <p>Womble is a word jumble game. Aim is to make as many 3-letter words as possible using the alphabets provided. 
-          To be able to move to the next level you need to make at least one word using all the alphabets.</p>
+          <p>Womble is a word jumble game. Aim is to make as many 3 or more letter words as possible using the letters provided. 
+          To be able to move to the next level you need to make at least one word using all the letters.</p>
           <h3>How are you scored?</h3>
           <ul>
             <li>Every word you make gets you 1 pont.</li>
-            <li>If the word uses all the available alphabets you get 10 points.</li>
+            <li>If the word uses all the available letters you get 10 points.</li>
             <li>If you are able to make half of the words or more in our list you get a 30 points bonus.</li>
             <li>If you are able to make all of the words in our list you get a 100 points bonus.</li>
           </ul>
